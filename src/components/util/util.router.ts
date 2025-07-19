a05a9beb198ff2e8fb1b84233abb98dc7705e7a7
Gabriel Herrera Z <gherreraz@lean-tech.io>
2025-07-19 07:09:53 -0500

    update version

--- src/components/util/util.router.ts
+++ src/components/util/util.router.ts
@@ -11,11 +11,6 @@ const utilRouter: Router = Router();
 utilRouter.get("/module/all", verifyToken, getAllModulesAvailable);
 utilRouter.post("/module/add-edit-module", verifyToken, addEditModule);
 
-utilRouter.get("/version", (__, res) => {
-  res.status(200).json({
-    version: "1.0.0",
-    message: "Welcome to the Event Management API",
-  });
-});
+utilRouter.get("/version", getAllModulesAvailable);
 
 export default utilRouter;
